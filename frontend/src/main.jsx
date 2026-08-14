dec09e7b59abacad92c33c50fccf312d425cdd9a
Preshita Shinde <preshitashinde09@gmail.com>
2026-08-14 16:43:54 +0400

    fix frontend architecture and providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { AuthProvider } from "./context/authcontext";
+import { CartProvider } from "./context/cartcontext";
 import App from "./app.jsx";
-
-import "./styles/variables.css";
 import "./styles/global.css";
-import "./styles/animations.css";
 import "./styles/app.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
