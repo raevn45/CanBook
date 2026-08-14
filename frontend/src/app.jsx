4adf1a5f2641f7e2aee218a850a42930de65738b
Preshita Shinde <preshitashinde09@gmail.com>
2026-08-14 17:28:57 +0400

    Add authenticated navigation with persistent sign-out

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,10 +1,23 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import AppRoutes from "./pages/routes/approutes";
+import Navbar from "./components/layout/navbar";
+
+function AppShell() {
+  const location = useLocation();
+  const isPublic = ["/", "/login", "/register"].includes(location.pathname);
+
+  return (
+    <>
+      {!isPublic && <Navbar />}
+      <AppRoutes />
+    </>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <AppShell />
     </BrowserRouter>
   );
 }
