bd8dab3330dd5cb6ccd2a175a2f4ca181590772d
Preshita Shinde <preshitashinde09@gmail.com>
2026-08-14 17:44:34 +0400

    Load global interaction styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { CartProvider } from "./context/cartcontext";
 import App from "./app.jsx";
 import "./styles/global.css";
 import "./styles/app.css";
+import "./styles/interactions.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
