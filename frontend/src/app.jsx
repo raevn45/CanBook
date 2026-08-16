1f25396ad8707f87cfd672f9675cb5657fdaa286
Preshita Shinde <preshitashinde09@gmail.com>
2026-08-16 13:42:25 +0400

    Load staff menu edit styling

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -4,24 +4,11 @@ import Navbar from "./components/layout/navbar";
 import InteractiveExperience from "./components/effects/interactiveexperience";
 import "./styles/staff-ui.css";
 import "./styles/force-menu-delete.css";
+import "./styles/menu-edit.css";
 
 function AppShell() {
   const location = useLocation();
   const isPublic = ["/", "/login", "/register"].includes(location.pathname);
-
-  return (
-    <>
-      <InteractiveExperience />
-      {!isPublic && <Navbar />}
-      <AppRoutes />
-    </>
-  );
+  return <><InteractiveExperience />{!isPublic && <Navbar />}<AppRoutes /></>;
 }
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <AppShell />
-    </BrowserRouter>
-  );
-}
+export default function App() { return <BrowserRouter><AppShell /></BrowserRouter>; }
